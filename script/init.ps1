9acf6401524dc5ec53b8d7b6596d8a23efef385b
sonatine726 <sonatine726@gmail.com>
2018-04-09 10:26:31 +0900

    Fix some syntax error.

---
+++ script/init.ps1
@@ -0,0 +1,3 @@
+docker-compose run --rm rails rails new /app --force --database=postgresql --skip-bundle --skip-test --skip-turbolinks
+cp database.yml app/config/database.yml
+docker-compose run --rm rails bundle exec spring binstub --all
