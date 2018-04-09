bb45beb0b623d186242075e83dcd98eacb94c9f2
sonatine726 <sonatine726@gmail.com>
2018-04-09 11:48:28 +0900

    - Change rails application directory name from "app" to "rails". - Make rails directory by init shell. - Delete old application directory "app".

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -1,3 +1,5 @@
+$ErrorActionPreference = "Stop"
+
 docker-compose run --rm spring rails db:create
 docker-compose run --rm spring rails db:migrate
 docker-compose up -d
--- script/init.ps1
+++ script/init.ps1
@@ -1,4 +1,6 @@
-cp Gemfile app/Gemfile
+$ErrorActionPreference = "Stop"
+mkdir rails
+cp Gemfile rails/Gemfile
 docker-compose run --rm rails rails new /app --force --database=postgresql --skip-bundle --skip-test --skip-turbolinks
-cp database.yml app/config/database.yml
+cp database.yml rails/config/database.yml
 docker-compose run --rm rails bundle exec spring binstub --all
