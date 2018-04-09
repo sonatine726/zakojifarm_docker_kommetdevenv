cee240d9634f8eee1b36832278fac8211b05eb41
sonatine726 <sonatine726@gmail.com>
2018-04-09 11:29:12 +0900

    Fix for first success of rails boot.

--- script/init.ps1
+++ script/init.ps1
@@ -1,3 +1,4 @@
+cp Gemfile app/Gemfile
 docker-compose run --rm rails rails new /app --force --database=postgresql --skip-bundle --skip-test --skip-turbolinks
 cp database.yml app/config/database.yml
 docker-compose run --rm rails bundle exec spring binstub --all
